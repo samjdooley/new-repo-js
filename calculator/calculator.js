4cee038335a72532e97fdc258760270eae55f7d8
Sam <samdooley1@hotmail.co.uk>
2020-05-19 23:09:16 +0100

    new commits

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,37 @@
 function add () {
-	
+	let sum = 0;
+	for (var i = 0; i < arguments.length; i++) {
+		sum += arguments[i]
+	} return sum
 }
 
-function subtract () {
-	
+function subtract (a,b) {
+	return a - b
 }
 
-function sum () {
-	
+function sum (arr) {
+	let sum = 0;
+	for (var i = 0; i < arr.length; i++) {
+		sum += arr[i]
+	} return sum 
 }
 
-function multiply () {
-	
+function multiply (arr) {
+	let multiple = 1;
+	for (var i = 0; i < arr.length; i++) {
+		multiple *= arr[i]
+	} return multiple 
 }
 
-function power() {
-	
+function power(a,b) {
+	return a**b
 }
 
-function factorial() {
-	
+function factorial(a) {
+	let factorial = 1
+	for (var i = 1; i <= a; i++) {
+		factorial *= i
+	} return factorial
 }
 
 module.exports = {
